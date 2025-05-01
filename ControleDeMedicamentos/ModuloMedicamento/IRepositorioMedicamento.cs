8704556bd705295cd34f95e5d9c5b3b40cf0b01d
vicentejrmc <vicente_pc@outlook.com>
2025-05-01 18:06:04 -0300

    Implementada Entidade Medicamento

---
+++ ControleDeMedicamentos/ModuloMedicamento/IRepositorioMedicamento.cs
@@ -0,0 +1,13 @@
+﻿using ControleDeMedicamentos.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeMedicamentos.ModuloMedicamento
+{
+    public interface IRepositorioMedicamento : IRepositorio<Medicamento>
+    {
+    }
+}
---
+++ ControleDeMedicamentos/ModuloMedicamento/Medicamento.cs
@@ -0,0 +1,54 @@
+﻿using ControleDeMedicamentos.Compartilhado;
+using ControleDeMedicamentos.ModuloFornecedor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeMedicamentos.ModuloMedicamento
+{
+    public class Medicamento : EntidadeBase<Medicamento>
+    {
+        public string NomeMedicamento { get; set; }
+        public string Descrição { get; set; }
+        public Fornecedor Fornecedor { get; set; }
+        public int QtdEstoque { get; set; }
+
+        public Medicamento(string nomeMedicamento, string descricao, Fornecedor fornecedor, int quantidade)
+        {
+            NomeMedicamento = nomeMedicamento;
+            Descrição = descricao;
+            Fornecedor = fornecedor;
+            QtdEstoque = quantidade;
+        }
+
+        public override void AtualizarRegistro(Medicamento resgitroEditado)
+        {
+            NomeMedicamento = resgitroEditado.NomeMedicamento;
+            Descrição = resgitroEditado.Descrição;
+        }
+
+        public override string Validar()
+        {
+            string ehValido = "";
+
+            //if (string.IsNullOrWhiteSpace(NomeMedicamento))
+            //    ehValido += "Erro! O campo Nome do Medicamento é obrigatório\n";
+
+            //if(NomeMedicamento.Length < 3 || NomeMedicamento.Length > 100)
+            //    ehValido += "Erro! O campo Nome do Medicamento deve ter entre 3 e 100 caracteres\n";
+
+            //if (string.IsNullOrWhiteSpace(Descrição))
+            //    ehValido += "Erro! O campo Descrição do Medicamento é obrigatório\n";
+
+            //if (Descrição.Length < 5 || Descrição.Length > 255)
+            //    ehValido += "Erro! O campo Descrição do Medicamento deve ter entre 3 e 100 caracteres\n";
+
+            //if (QtdEstoque < 0)
+            //    ehValido += "Erro! O campo Qtd Estoque do Medicamento não pode ser < 0\n";
+
+            return ehValido;
+        }
+    }
+}
