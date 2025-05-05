1e6f81de60d70728499a72dab4513f29528f3b3f
vicentejrmc <vicente_pc@outlook.com>
2025-05-05 09:52:46 -0300

    Ajustes na Validação CNPJ do Cadastro de Fornecedor

--- ControleDeMedicamentos/ModuloFornecedor/Fornecedor.cs
+++ ControleDeMedicamentos/ModuloFornecedor/Fornecedor.cs
@@ -26,19 +26,19 @@ public class Fornecedor : EntidadeBase<Fornecedor>
         if (string.IsNullOrEmpty(Nome))
             erros += "Erro! O campo Nome é obrigatório.\n";
 
-        else if (Nome.Length < 3 || Nome.Length > 100)
+        if (Nome.Length < 3 || Nome.Length > 100)
             erros += "Erro! O campo Nome deve ter entre 3 e 100 caracteres.\n";
 
         if (string.IsNullOrEmpty(CNPJ))
             erros += "Erro! O campo CNPJ é obrigatório.\n";
 
-        else if (CNPJ.Length != 13)
-            erros += "Erro! O campo CNPJ deve ter exatamente 13 caracteres.\n";
+        if (!Regex.IsMatch(CNPJ, @"^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$"))
+            erros += "Erro! O campo CNPJ deve estar no formato XX.XXX.XXX/XXXX-XX.\n";
 
         if (string.IsNullOrEmpty(Telefone))
             erros += "Erro! O campo Telefone é obrigatório.\n";
 
-        else if (!Regex.IsMatch(Telefone, @"^\(\d{2}\) \d{4,5}-\d{4}$"))
+        if (!Regex.IsMatch(Telefone!.ToString(), @"^\d{2}\ \d{4,5}-\d{4}$"))
             erros += "Erro! O campo Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX.\n";
 
         return erros;
